1b7bf2bed388e88bb35fb554d6e10b017232e8c0
jwlarocque <jwlarocque@gmail.com>
2016-02-03 09:57:09 -0600

    partially working border generation

--- js/border.js
+++ js/border.js
@@ -1,9 +1,18 @@
 var images = ["../img/c1.png", "../img/c2.png", "../img/c3.png", "../img/c4.png", "../img/c5.png",
               "../img/c7.png", "../img/c8.png", "../img/c9.png", "../img/c10.png", "../img/c11.png",
               "../img/c12.png", "../img/c13.png", "../img/c14.png"]
+var viewportWidth = $(window).width();
+var viewportHeight = $(window).height();
+const spriteHeight = 100;
+
+
 window.onload = function() {
-    this.img = document.createElement("img");
-    this.img.src = 'url(images/' + images[Math.floor(Math.random() * images.length)];
-    src = document.getElementById('content');
-    src.appendChild(this.img);
+    var rightArray = [];
+    for (i = 0; i < viewportHeight / spriteHeight; i++) {
+        this.img = document.createElement("img");
+        this.img.src = 'url(/' + images[Math.floor(Math.random() * images.length)];
+        this.img.style.height = spriteHeight.toString() + "px";
+        parent = document.getElementById("left");
+        parent.appendChild(this.img);
+    }
 }
