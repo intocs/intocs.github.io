e3d083cd65fb2f255d7b7f3998663b0bb174e62e
Chris Winkler <quidnovum@gmail.com>
2016-02-03 10:39:11 -0600

    Cleaned up some HTML and the image name list

--- js/border.js
+++ js/border.js
@@ -1,25 +1,29 @@
-var images = ["../img/c1.png", "../img/c2.png", "../img/c3.png", "../img/c4.png", "../img/c5.png",
-              "../img/c7.png", "../img/c8.png", "../img/c9.png", "../img/c10.png", "../img/c11.png",
-              "../img/c12.png", "../img/c13.png"]
+const NUM_IMAGES = 13;
+var images = [];
+
+for (var i = 0; i < 13; i++) {
+    images.push(`../img/c${i + 1}.png`);
+}
+
 var viewportWidth = $(window).width();
 var viewportHeight = $(window).height();
+
 const spriteHeight = viewportWidth / (5 * 3);
 
 
 window.onload = function() {
     for (i = 0; i < viewportHeight / spriteHeight; i++) {
-        this.img = document.createElement("img");
-        this.img.src = 'url(/' + images[Math.floor(Math.random() * images.length)];
-        this.img.style.height = spriteHeight.toString() + "px";
+        var img = document.createElement("img");
+        img.src = 'url(/' + images[Math.floor(Math.random() * images.length)];
+        img.style.height = spriteHeight.toString() + "px";
         parent = document.getElementById("left");
-        parent.appendChild(this.img);
-    }
-    for (i = 0; i < viewportHeight / spriteHeight; i++) {
-        this.img = document.createElement("img");
-        this.img.src = 'url(/' + images[Math.floor(Math.random() * images.length)];
-        this.img.style.height = spriteHeight.toString() + "px";
+        parent.appendChild(img);
+
+        img = document.createElement("img");
+        img.src = 'url(/' + images[Math.floor(Math.random() * images.length)];
+        img.style.height = spriteHeight.toString() + "px";
         this.img.className = "border-sprite";
         parent = document.getElementById("right");
-        parent.appendChild(this.img);
+        parent.appendChild(img);
     }
 }
